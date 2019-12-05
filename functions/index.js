983be4c837de079a6287d3b2f88853afe2a74706
Brinda Rathod <pinky29196@gmail.com>
2019-12-05 17:14:03 +0530

    yi

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+var admin = require("firebase-admin");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+var serviceAccount = require("path/to/serviceAccountKey.json");
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Mirinda!");
 });
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://all-unit-converter-cedbc.firebaseio.com"
+});
+
